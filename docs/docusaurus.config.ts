3e492b2a8a27c53eb1572af7de4504d72fbf0abc
Fatima <fatima787asif@gmail.com>
2025-12-19 12:27:59 +0500

    add few changing

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -32,7 +32,7 @@ const config: Config = {
 
   // Custom fields for API configuration (used in your hooks)
   customFields: {
-    apiUrl: process.env.REACT_APP_API_URL || 'https://ai-rative-book-backend-production.up.railway.app',
+    apiUrl: process.env.REACT_APP_API_URL || 'hackathon-physical-ai-robotics-production.up.railway.app',
     apiKey: process.env.REACT_APP_API_KEY || '',
   },
 
